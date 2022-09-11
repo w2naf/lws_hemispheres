4e1e0ca0c2a017ac75df8beae1c7f7e28ceb5a8e
Nathaniel A. Frissell <nathaniel.frissell@scranton.edu>
2022-09-11 08:04:33 -0400

    implement multiprocessing

--- plot_hemi.py
+++ plot_hemi.py
@@ -14,6 +14,8 @@ import matplotlib as mpl
 mpl.use('Agg')
 from matplotlib import pyplot as plt
 
+import multiprocessing
+
 mpl.rcParams['font.size']        = 16
 mpl.rcParams['font.weight']      = 'bold'
 mpl.rcParams['axes.labelweight'] = 'bold'
@@ -22,6 +24,28 @@ mpl.rcParams['axes.grid']        = True
 mpl.rcParams['grid.linestyle']   = ':'
 #mpl.rcParams['figure.figsize']   = np.array([15, 8])
 mpl.rcParams['axes.xmargin']     = 0
+
+class ScriptTimer(object):
+    def __init__(self):
+        self.sTime  = datetime.datetime.now()
+        self.script = os.path.basename(__file__)
+
+        print('{!s} Running...'.format(self.script))
+        print('   Started: {!s}'.format(self.sTime))
+        print()
+
+    def stop(self):
+        self.eTime  = datetime.datetime.now()
+        total       = self.eTime - self.sTime
+
+        print()
+        print('#--------------------------------------#')
+        print('{!s} Finished...'.format(self.script))
+        print('   Started:  {!s}'.format(self.sTime))
+        print('   Finished: {!s}'.format(self.eTime))
+        print('   Duration: {!s}'.format(total))
+        print('#--------------------------------------#')
+        print()
 
 def date_range(date_0,date_1):
     """
@@ -50,7 +74,7 @@ def gen_radar_list():
 
     return rads
 
-def load_fitacf(sTime,eTime,radar,fit_sfx='fitacf'):
+def load_fitacf(sTime,eTime,radar,sd_path='/data/sd-data',fit_sfx='fitacf'):
     """
     Load FITACF data from multiple FITACF files by specifying a date range.
 
@@ -70,7 +94,7 @@ def load_fitacf(sTime,eTime,radar,fit_sfx='fitacf'):
     for date in dates:
         date_str        = date.strftime('%Y%m%d')
 #        /sd-data/2021/fitacf/bks
-        data_dir    = os.path.join('/sd-data',date.strftime('%Y'),fit_sfx,radar)
+        data_dir    = os.path.join(sd_path,date.strftime('%Y'),fit_sfx,radar)
         fpattern        = os.path.join(data_dir,'{!s}*{!s}*.{!s}.bz2'.format(date_str,radar,fit_sfx))
         fitacf_paths_0   += glob.glob(fpattern)
 
@@ -135,7 +159,7 @@ def plot_hemisphere(sTime,eTime=None,hemisphere='south',beam=7,ymin=200,ymax=300
 
         fitacf      = load_fitacf(sTime,eTime,radar)
         try:
-            pydarn.RTP.plot_range_time(fitacf, beam_num=beam, parameter='p_l', zmin=zmin, zmax=zmax, cmap='jet',
+            pydarn.RTP.plot_range_time(fitacf, beam_num=beam, parameter='p_l', zmin=zmin, zmax=zmax, cmap=None,
                     date_fmt='%H%M', colorbar_label='Power (dB)', ax=ax)
         except:
             ax.axis('off')
@@ -159,7 +183,10 @@ def plot_hemisphere(sTime,eTime=None,hemisphere='south',beam=7,ymin=200,ymax=300
 
 
     fmt   = '%Y %b %d %H%M UT'
-    txt = ('{!s}ern Hemisphere {!s} - {!s}'.format(hemisphere.title(),sTime.strftime(fmt),eTime.strftime(fmt)))
+    if (eTime - sTime) == datetime.timedelta(days=1):
+        txt = ('{!s}ern Hemisphere {!s}'.format(hemisphere.title(),sTime.strftime(fmt)))
+    else:
+        txt = ('{!s}ern Hemisphere {!s} - {!s}'.format(hemisphere.title(),sTime.strftime(fmt),eTime.strftime(fmt)))
     fig.text(0.5,1.010,txt,fontdict={'weight':'bold','size':44},ha='center')
 
     fig.tight_layout()
@@ -171,9 +198,13 @@ def plot_hemisphere_dict(run_dict):
     return plot_hemisphere(**run_dict)
 
 if __name__ == '__main__':
+    timer = ScriptTimer()
+
+    multiproc   = True
+    ncpus       = multiprocessing.cpu_count()
 
     date_0      = datetime.datetime(2017,11,1)
-    date_1      = datetime.datetime(2017,11,3)
+    date_1      = datetime.datetime(2017,11,11)
     dates       = date_range(date_0,date_1)
 
     hemisphere  = 'south'
@@ -193,7 +224,13 @@ if __name__ == '__main__':
 
         run_dicts.append(rd)
 
-    for rd in run_dicts:
-        plot_hemisphere_dict(rd)
-#    import ipdb; ipdb.set_trace()
-#    plot_hemisphere(sTime,hemisphere=hemisphere,output_dir=output_dir)
+    ## Load and plot SuperDARN Data
+    if multiproc:
+        with multiprocessing.Pool(ncpus) as pool:
+            pool.map(plot_hemisphere_dict,run_dicts)
+    else:
+        # Single Processor
+        for run_dict in run_dicts:
+            fpath = plot_hemisphere_dict(run_dict)
+
+    timer.stop()
