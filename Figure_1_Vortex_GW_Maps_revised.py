5f09a32989fe24009457359406608afb5ad9122e
Nathaniel A. Frissell <nathaniel.frissell@scranton.edu>
2024-04-21 14:01:52 -0400

    hovmoller

---
+++ Figure_1_Vortex_GW_Maps_revised.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python
+"""
+Figure_1_Vortex_GW_Maps.py
+Nathaniel A. Frissell
+February 2024
+
+This script is used to generate Figure 1 of the Frissell et al. (2024)
+GRL manuscript on multi-instrument measurements of AGWs, MSTIDs, and LSTIDs.
+"""
+
+import os
+import shutil
+import glob
+import string
+
+import datetime
+
+import numpy as np
+import scipy as sp
+import pandas as pd
+import xarray as xr
+
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+from matplotlib.collections import PolyCollection
+
+import cartopy.crs as ccrs
+from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
+import cartopy.feature as cfeature
+
+import AIRS3D
+import merra2AirsMaps
+import hovmoller
+
+pd.set_option('display.max_rows', None)
+
+mpl.rcParams['font.size']      = 22
+mpl.rcParams['font.weight']    = 'bold'
+mpl.rcParams['axes.titleweight']   = 'bold'
+mpl.rcParams['axes.labelweight']   = 'bold'
+mpl.rcParams['axes.grid']      = True
+mpl.rcParams['grid.linestyle'] = ':'
+mpl.rcParams['figure.figsize'] = np.array([15, 9])
+mpl.rcParams['axes.xmargin']   = 0
+    
+if __name__ == "__main__":
+    output_dir = os.path.join('output','Fig1_Vortex_GW_Maps')
+    if os.path.exists(output_dir):
+        shutil.rmtree(output_dir)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    mca = merra2AirsMaps.Merra2AirsMaps()
+
+    # Create a dictionary with information about each row, especially
+    # the base filename of the AIRS3D profile data file we want to use.
+    rows_dct = {}
+    rowd = rows_dct[datetime.datetime(2018,12,10)] = {}
+    rowd['a3d_prof_bname']   = 'NEW_2018 12 10_AIRS_3D_alt_data_45_deg_lat'
+    rowd['a3d_prof_lat']     = 45
+
+    rowd = rows_dct[datetime.datetime(2019,1,5)] = {}
+    rowd['a3d_prof_bname']   = 'NEW_2019 01 05_AIRS_3D_alt_data_50_deg_lat'
+    rowd['a3d_prof_lat']     = 50
+
+    rowd = rows_dct[datetime.datetime(2019,2,1)] = {}
+    rowd['a3d_prof_bname']   = 'NEW_2019 02 01_AIRS_3D_alt_data_48_deg_lat'
+    rowd['a3d_prof_lat']     = 48
+
+    png_name    = 'Fig1_Vortex_GW_Maps_revised.png'
+    png_fpath   = os.path.join(output_dir,png_name)
+
+    figsize = (22.5,24)
+    fig     = plt.figure(figsize=figsize)
+
+    col_fwidths     = [0.35,0.35,0.40]
+    ncols           = len(col_fwidths)
+
+    nrows           = len(rows_dct)
+    row_heights     = 1./nrows
+    row_pad         = 0.175*row_heights
+
+
+    # Create array of letters for labeling panels.
+    letters = np.array(list(string.ascii_lowercase[:nrows*(ncols-1)]))
+    letters.shape = (ncols-1,nrows)
+    letters = letters.T
+
+    cbar_left   = 1.
+    cbar_width  = 0.015
+
+    # Longitude at bottom of map.
+    lon_down = -90
+
+    cbar_bottom = -0.05
+    cbar_height =  0.025
+
+    title_fontdict  = {'weight':'bold','size':32}
+    letter_fontdict = {'weight':'bold','size':32}
+    for row_inx,(date,rowd) in enumerate(rows_dct.items()):
+        print(date)
+        # fig.add_axes([left,bottom,width,height])
+        bottom  = 1. - row_heights*(row_inx+1)
+        height  = row_heights - row_pad
+
+        print(' --> Plotting Vortex Map')
+        col_inx = 0
+        left    = np.sum(col_fwidths[:col_inx])
+        width   = col_fwidths[col_inx]
+
+        ax      = fig.add_axes([left,bottom,width,height],projection=ccrs.Orthographic(lon_down,90))
+        AIRS_GWv_vmin       = 0.
+        AIRS_GWv_vmax       = 0.8
+        # Loosely dashed negative linestyle
+        # See https://matplotlib.org/stable/gallery/lines_bars_and_markers/linestyles.html
+        merra2_vortex_kw    = {'linewidths':3,'negative_linestyles':(0, (1, 3))}
+        result  = mca.plot_ax(ax,date,vmin=AIRS_GWv_vmin,vmax=AIRS_GWv_vmax,cmap='RdPu',
+                merra2_vortex_kw=merra2_vortex_kw)
+        title   = date.strftime('%d %b %Y')
+        ax.set_title(title,pad=18,fontdict=title_fontdict)
+
+        letter = '({!s})'.format(letters[row_inx,col_inx])
+        ax.text(0.025,0.95,letter,transform=ax.transAxes,fontdict=letter_fontdict)
+
+        if row_inx == nrows-1:
+            cbar_width  = 0.8*width
+            cbar_left   = left + (width-cbar_width)/2.
+            cax     = fig.add_axes([cbar_left,cbar_bottom,cbar_width,cbar_height])
+            cax.grid(False)
+            AIRS_cbar_ticks = np.arange(AIRS_GWv_vmin,AIRS_GWv_vmax+0.1,0.1)
+            cbar    = fig.colorbar(result['cbar_pcoll'],cax=cax,ticks=AIRS_cbar_ticks,orientation='horizontal')
+            cbar.set_label(result['cbar_label'])
+
+        profile_lat     = rowd['a3d_prof_lat']
+
+        print(' --> Loading AIRS3D Data')
+        a3dw    = AIRS3D.AIRS3DWorld(date)
+        a3dlp   = AIRS3D.AIRS3DLatProfile(rowd['a3d_prof_bname'],date,lat=profile_lat)
+
+        # Determine Longitude Boundaries for region where there is valid data.
+        tf              = np.isfinite(a3dlp.Tpert)
+        LONS            = a3dlp.lons.copy()
+        LONS.shape      = (len(LONS),1)
+        LONS            = LONS*np.ones_like(tf,dtype=float)
+        profile_lons    = [np.min(LONS[tf]),np.max(LONS[tf])]
+
+        print(' --> Plotting AIRS Global Temperature Perturbation Map')
+        col_inx += 1
+        left    = np.sum(col_fwidths[:col_inx])
+        width   = col_fwidths[col_inx]
+        ax      = fig.add_axes([left,bottom,width,height],projection=ccrs.Orthographic(lon_down,90))
+        result  = a3dw.plot_ax(ax=ax,vmin=-0.5,vmax=0.5)
+        mca.overlay_windspeed(ax,date)
+        mca.overlay_vortex(ax,date,merra2_vortex_kw=merra2_vortex_kw)
+        ax.set_title(title,pad=18,fontdict=title_fontdict)
+
+        letter = '({!s})'.format(letters[row_inx,col_inx])
+        ax.text(0.025,0.95,letter,transform=ax.transAxes,fontdict=letter_fontdict)
+
+        if row_inx == nrows-1:
+            cbar_width  = 0.8*width
+            cbar_left   = left + (width-cbar_width)/2.
+            cax     = fig.add_axes([cbar_left,cbar_bottom,cbar_width,cbar_height])
+            cax.grid(False)
+            cbar    = fig.colorbar(result['cbar_pcoll'],cax=cax,extend='both',orientation='horizontal')
+            cbar.set_label(result['cbar_label'])
+
+    ################################################################################ 
+    # Hovmoller Diagram
+    col_inx += 1
+    left    = np.sum(col_fwidths[:col_inx])
+    width   = col_fwidths[col_inx]
+    bottom  = 0
+    height  = 1
+    ax = fig.add_axes([left,bottom,width,height])
+
+    hov     = hovmoller.Hovmoller()
+    vmin    = 0.
+    vmax    = 1.
+    result  = hov.plot_ax(ax,vmin=vmin,vmax=vmax)
+
+    cbar_width  = 0.8*width
+    cbar_left   = left + (width-cbar_width)/2.
+    cax     = fig.add_axes([cbar_left,cbar_bottom,cbar_width,cbar_height])
+    cax.grid(False)
+    cbar    = fig.colorbar(result['cbar_pcoll'],cax=cax,extend='both',orientation='horizontal')
+    cbar.set_label(result['cbar_label'])
+    dtick   = 0.2
+    cax.set_xticks(np.arange(vmin,vmax+dtick,dtick))
+
+    fig.savefig(png_fpath,bbox_inches='tight')
+    plt.close(fig)
+    print(png_fpath)
+
---
+++ hovmoller.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python
+import os
+import shutil
+import datetime
+
+import numpy as np
+import pandas as pd
+import scipy as sp
+import scipy.ndimage
+
+import xarray as xr
+
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+
+from PIL import Image
+
+import cartopy.crs as ccrs
+from cartopy.util import add_cyclic_point
+
+mpl.rcParams['font.size']      = 12
+mpl.rcParams['font.weight']    = 'bold'
+mpl.rcParams['axes.grid']      = True
+mpl.rcParams['grid.linestyle'] = ':'
+mpl.rcParams['figure.figsize'] = np.array([15, 8])
+mpl.rcParams['axes.xmargin']   = 0
+
+def to_cmap(my_cdict,name='CustomCMAP',vmin=-2.,vmax=1.):
+    norm = mpl.colors.Normalize(vmin=vmin,vmax=vmax)
+    
+    red   = []
+    green = []
+    blue  = []
+    
+    keys = list(my_cdict.keys())
+    keys.sort()
+    
+    for x in keys:
+        r,g,b, = my_cdict[x]
+        x = norm(x)
+        r = r/255.
+        g = g/255.
+        b = b/255.
+        red.append(   (x, r, r))
+        green.append( (x, g, g))
+        blue.append(  (x, b, b))
+    cdict = {'red'   : tuple(red),
+             'green' : tuple(green),
+             'blue'  : tuple(blue)}
+    cmap  = mpl.colors.LinearSegmentedColormap(name, cdict)
+    return cmap
+
+def rainbowCmap():
+    #Color Map
+    img = Image.open('rainbow.png')
+    data_2=np.array(img)
+
+    vmin, vmax = (-2, 1)
+    inxs = (np.linspace(vmin,vmax,data_2.shape[1])).tolist()
+    vals = [tuple(x) for x in (data_2[0,:,:]).tolist()]
+
+    rgb_dict = {}
+    for inx,val in zip(inxs,vals):
+        rgb_dict[inx] = val
+
+    cmap = to_cmap(rgb_dict,vmin=vmin,vmax=vmax)
+    return cmap
+
+class Hovmoller(object):
+    def __init__(self):
+        self.load_data()
+    
+    def load_data(self):
+        data_dir    = os.path.join('data','hovmoller_airs_merra2')
+        sav_name    = 'xt_airs_variance+merra2_vortex_speed_20181101-20190501_50.sav'
+        sav_path    = os.path.join(data_dir,sav_name)
+        sav_data    = sp.io.readsav(sav_path)
+
+        self.sav_data   = sav_data
+
+        return sav_data
+
+    def plot_figure(self,png_fpath='hovmoller.png',figsize=(8,12),**kwargs):
+
+        fig     = plt.figure(figsize=figsize)
+        ax          = fig.add_subplot(1,1,1)
+        result      = self.plot_ax(ax,**kwargs)
+
+#        cbar        = fig.colorbar(result['cbar_pcoll'],aspect=15,shrink=0.8,location='bottom')
+        cbar        = fig.colorbar(result['cbar_pcoll'],location='bottom',pad=0.05)
+        cbar.set_label(result['cbar_label'],fontdict={'weight':'bold','size':20})
+
+        fig.tight_layout()
+        fig.savefig(png_fpath,bbox_inches='tight')
+        plt.close(fig)
+    
+    def plot_ax(self,ax,vmin=0.,vmax=1,cmap='IDL',
+            merra2_windspeed_kw={},
+            ylim = (datetime.datetime(2019,3,1),datetime.datetime(2018,12,1)),
+            **kwargs):
+        """
+        merra2_windspeed_kw: Keywords passed to ax.contour() for merra2_windspeed.
+        """
+
+        fig     = ax.get_figure()
+
+        if cmap == 'IDL':
+            cmap    = rainbowCmap()
+
+        sav_data = self.sav_data
+
+        airs_lons   = sav_data['xnew_wrap']
+        dates       = [datetime.datetime.strptime(x.decode(), '%Y%m%d') for x in sav_data['sdate_all']]
+
+        zz      = sav_data['variance_lineplot_avg'].copy()
+        tf      = ~np.isfinite(zz)
+        zz[tf]  = 0.
+        zz[zz < 0] = 0.
+#        zz[zz < 0.01] = np.nan
+
+        cbar_pcoll  = ax.contourf(airs_lons,dates,zz,
+                cmap=cmap,levels=np.arange(vmin,vmax+0.005,0.005),extend='max')
+        cbar_label  = 'AIRS GW Variance [K$^{2}$]'
+
+        # MERRA2 Wind Speed ############################################################ 
+        # Default contour and coastline style parameters.
+        # Tuned for an IDL-colormap style plot.
+
+        m2ws = {} # MERRA2 Wind Stream Parameters
+#        m2ws['levels']      = [-10000,40,60]
+#        m2ws['colors']      = ['white','orange','red']
+        m2ws['levels']              = [-20,0,20,50]
+        m2ws['colors']              = ['0.5','0.5','orange','red']
+        m2ws['linewidths']          = 3
+        m2ws['zorder']              = 1000
+        m2ws.update(merra2_windspeed_kw)
+
+        merra2_lons             = sav_data['alon_save']
+        merra2_windspeed        = sav_data['u_lineplot_avg']
+
+        WS = ax.contour(merra2_lons,dates,merra2_windspeed,**m2ws)
+
+		# Override the linestyles based on the levels.
+        for line, lvl in zip(WS.collections, WS.levels):
+            if lvl < 0:
+                line.set_linestyle('--')
+            elif lvl == 0:
+                line.set_linestyle(':')
+            else:
+                # Optional; this is the default.
+                line.set_linestyle('-')
+
+        try:
+            ax.clabel(WS,inline=True)
+        except:
+            pass
+
+        ax.set_ylim(ylim)
+
+        result  = {}
+        result['cbar_pcoll']    = cbar_pcoll
+        result['cbar_label']    = cbar_label
+        return result
+
+if __name__ == '__main__':
+    output_dir = os.path.join('output','hovmoller')
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    hov = Hovmoller()
+
+    png_fname   = 'hovmoller.png'
+    png_fpath   = os.path.join(output_dir,png_fname)
+    hov.plot_figure(png_fpath=png_fpath)
+    print(png_fpath)
