6f2fb6de1c6fd197487809996711f94f7927bad8
Nathaniel A. Frissell <nathaniel.frissell@scranton.edu>
2024-08-10 00:00:26 +0000

    now plotting maps

--- ham_spot_plot.py
+++ ham_spot_plot.py
@@ -9,10 +9,12 @@ import pickle
 
 import numpy as np
 import pandas as pd
+import xarray as xr
 
 import matplotlib as mpl
 mpl.use('Agg')
 from matplotlib import pyplot as plt
+import cartopy.crs as ccrs
 
 mpl.rcParams['font.size']      = 12
 mpl.rcParams['font.weight']    = 'bold'
@@ -20,6 +22,28 @@ mpl.rcParams['axes.grid']      = True
 mpl.rcParams['grid.linestyle'] = ':'
 mpl.rcParams['figure.figsize'] = np.array([15, 8])
 mpl.rcParams['axes.xmargin']   = 0
+
+# Region Dictionary
+regions = {}
+tmp     = {}
+tmp['lon_lim']  = (-180.,180.)
+tmp['lat_lim']  = ( -90., 90.)
+regions['World']    = tmp
+
+tmp     = {}
+tmp['lon_lim']  = (-55.,-10)
+tmp['lat_lim']  = (  40., 65.)
+regions['Atlantic_Ocean']   = tmp
+
+tmp     = {}
+tmp['lon_lim']  = (-130.,-60.)
+tmp['lat_lim']  = (  20., 55.)
+regions['US']   = tmp
+
+def get_bins(lim, bin_size):
+    """ Helper function to split a limit into bins of the proper size """
+    bins    = np.arange(lim[0], lim[1]+1*bin_size, bin_size)
+    return bins
 
 def fmt_xaxis(ax,xlim=None,label=True):
     ax.xaxis.set_major_locator(mpl.dates.HourLocator(interval=1))
@@ -33,9 +57,10 @@ class HamSpotPlot(object):
             data_dir    = os.path.join('data','lstid_ham')
         self.data_dir   = data_dir
         self.date       = date
-        self.load_data()
+        self.load_geo_data()
+        self.load_edge_data()
     
-    def load_data(self):
+    def load_edge_data(self):
         date    = self.date
 
         # EDGE DETECT DATA #####################
@@ -48,18 +73,83 @@ class HamSpotPlot(object):
             edge_data   = pickle.load(pkl)
 
         self.edge_data  = edge_data
-        # hamSpot_geo_2018_12_15.csv.bz2
+        return edge_data
 
-    def plot_timeSeries_figure(self,png_fpath='output.png',figsize=(16,5),**kwargs):
+    def load_geo_data(self,dlat=1,dlon=1):
+        # hamSpot_geo_2018_12_15.csv.bz2
+        date_str        = date.strftime('%Y_%m_%d')
+        fname           = f'hamSpot_geo_{date_str}.csv.bz2'
+        fpath           = os.path.join(self.data_dir,fname)
+        self.geo_fpath  = fpath
+        geo_df          = pd.read_csv(fpath)
+
+        xkey    = 'lon'
+        ykey    = 'lat'
+
+        xlim    = (-180,180)
+        ylim    = ( -90, 90)
+
+        xbins   = get_bins(xlim,dlon)
+        ybins   = get_bins(ylim,dlat)
+
+        xvals   = geo_df[xkey].values
+        yvals   = geo_df[ykey].values
+        hist, xb, yb = np.histogram2d(xvals,yvals, bins=[xbins, ybins])
+
+        crds    = {}
+        crds[xkey]          = xb[:-1]
+        crds[ykey]          = yb[:-1]
+
+        attrs   = {}
+        attrs['xkey']   = xkey
+        attrs['ykey']   = ykey
+        
+        geo_hist    = xr.DataArray(hist,crds,attrs=attrs,dims=[xkey,ykey])
+        self.geo_df     = geo_df
+        self.geo_hist   = geo_hist
+
+    def plot_figure(self,png_fpath='output.png',figsize=(16,10),**kwargs):
 
         fig     = plt.figure(figsize=figsize)
-        ax      = fig.add_subplot(1,1,1)
 
+        self.plot_map_ax(fig,subplot=(2,1,1))
+
+        ax      = fig.add_subplot(2,1,2)
         result  = self.plot_timeSeries_ax(ax,**kwargs)
 
         fig.tight_layout()
         fig.savefig(png_fpath,bbox_inches='tight')
         plt.close(fig)
+
+    def plot_map_ax(self,fig,subplot=None,
+            plot_region='US'):
+        ax      = fig.add_subplot(*subplot,projection=ccrs.PlateCarree())
+
+        # Plot Map #############################
+        ax.coastlines(zorder=10,color='w')
+        map_data        = self.geo_hist
+        map_data.name   = '14 MHz Midpoints'
+
+        tf          = map_data < 1
+        map_n       = int(np.sum(map_data))
+        map_data    = np.log10(map_data)
+        map_data.values[tf] = 0
+        map_data.name   = 'log({})'.format(map_data.name)
+        map_data.plot.contourf(x=map_data.attrs['xkey'],y=map_data.attrs['ykey'],ax=ax,levels=30,cmap=mpl.cm.inferno)
+        ax.set_title('')
+        lweight = mpl.rcParams['axes.labelweight']
+        lsize   = mpl.rcParams['axes.labelsize']
+        fdict   = {'weight':lweight,'size':lsize}
+        ax.text(0.5,-0.1,'Radio Spots (N = {!s})'.format(map_n),
+                ha='center',transform=ax.transAxes,fontdict=fdict)
+
+        if plot_region is not None:
+            rgn         = regions.get(plot_region)
+            lat_lim     = rgn.get('lat_lim')
+            lon_lim     = rgn.get('lon_lim')
+
+            ax.set_xlim(lon_lim)
+            ax.set_ylim(lat_lim)
     
     def plot_timeSeries_ax(self,ax,cb_pad=0.125,plot_fit=True):
         fig             = ax.get_figure()
@@ -127,5 +217,5 @@ if __name__ == '__main__':
 
     png_fname   = date.strftime('%Y%m%d')+'_hamSpotPlot.png'
     png_fpath   = os.path.join(output_dir,png_fname)
-    hsp.plot_timeSeries_figure(png_fpath=png_fpath)
+    hsp.plot_figure(png_fpath=png_fpath)
     print(png_fpath)
